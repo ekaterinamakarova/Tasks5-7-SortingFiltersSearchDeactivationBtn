84978a64240c9e8f105d83dfd8e901c22c5a23bb
Ekaterina Makarova <44766780+ekaterinamakarova@users.noreply.github.com>
2019-02-07 17:11:24 +0300

    Update TestClass.java

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,61 +1,110 @@
-package Pages;
+import Pages.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+import java.io.IOException;
 
-public class Initial {
+public class TestClass {
 
-    @FindBy(css = "a[href='/welcome/sign-in']") private WebElement signStr;
-    @FindBy(css = "svg[title='Manage Groups']") private WebElement manageGroupsIcon;
-    @FindBy(css="[title='Calendar Notes']") private WebElement calendarNotesIcon;
-    @FindBy(xpath = "//header[1]/div[1]/div[2]/button[1]") private WebElement adminButton;
-    @FindBy(xpath = "//a[contains(text(),'Users')]") private WebElement usersButton;
-    @FindBy(xpath = "//a[contains(text(),'Actions Log')]") private WebElement actionsLogButton;
-    @FindBy(xpath = "//li[contains(text(),'Logout')]") private WebElement logOutButton;
-    @FindBy(css = "svg[title='Price Calendar']") private WebElement priceCalendarButton;
+    private ChromeDriver driver;
 
-    WebDriver driver;
-    HelperClass helper;
+    FileReaderClass readerClass = new FileReaderClass();
+    private Initial initial;
+    private SignIn signIn;
+    private Users users;
+    private ActionsLog actionsLog;
+    private PriceCalendar priceCalendar;
 
-    public  Initial(WebDriver driver){
-        this.driver=driver;
-        PageFactory.initElements(driver,this);
-        helper = new HelperClass(driver);
-        helper.implicitWait(driver);
+    @BeforeClass
+    public void setUp() throws IOException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(CapabilityType.PAGE_LOAD_STRATEGY, "normal");
+        options.addArguments("start-maximized");
+        options.addArguments("--no-sandbox");
+        driver = new ChromeDriver(options);
+        driver.get(readerClass.readFromFile(0));
+        initial = new Initial(driver);
+        signIn = new SignIn(driver);
+        users= new Users(driver);
+        actionsLog = new ActionsLog(driver);
+        priceCalendar =  new PriceCalendar(driver);
+
     }
 
-    public void toSignPage(){
-        signStr.click();
+    @Test(description = "Login with credentials")
+    public void login () throws IOException {
+        initial.toSignPage();
+        signIn.signin(readerClass.readFromFile(1),readerClass.readFromFile(2));
+    }
+
+//    @Test(description = "Users page sorting checking", dependsOnMethods = {"login"})
+//    public void usersChecking() throws  InterruptedException, AWTException {
+//        initial.toUsersPage();
+//        driver.navigate().refresh();
+//        users.sort();
+//    }
+//
+//    @Test(description = "Filters checking at Users page", dependsOnMethods = {"usersChecking"})
+//    public void filterChecking() throws InterruptedException, AWTException {
+//        users.allFilters();
+//        users.specialOfferFilter();
+//        users.hotel_activeFilter();
+//        users.hotel_suspendedFilter();
+//        users.tourOperator_suspendedFilter();
+//        users.tourOperator_activeFilter();
+//    }
+//
+//    @Test(description = "Search checking at Users page", dependsOnMethods = {"filterChecking"})
+//    public void searchChecking() throws InterruptedException {
+//        driver.navigate().refresh();
+//        users.searchCheck("123", "FLEXAR", "HOTEL");
+//    }
+
+//    @Test(description = "Actions Log page's sorting and search checking", dependsOnMethods = {"searchChecking"})
+//    public void actionsLogCheck() throws InterruptedException, AWTException {
+//        initial.toActionsLog();
+//        actionsLog.sorting();
+//        actionsLog.search("31", "Super Admin");
+//    }
+
+    @Test(description = "Deactivation of special offer", dependsOnMethods = {"login"})
+    public void deactivateOffer() throws InterruptedException, IOException {
+        initial.toUsersPage();
+        users.search("autoAccountHO@autoAccountHO.com");
+        users.activationDeaktivation();
+        initial.logOut();
+        initial.toSignPage();
+        signIn.signin(readerClass.readFromFile(3),readerClass.readFromFile(4));
+        initial.toPriceCalendar();
+        priceCalendar.checkButtonDeactivation();
+    }
+
+    @Test(description = "Activation of special offer", dependsOnMethods = {"deactivateOffer"})
+    public void activateOffer() throws InterruptedException, IOException {
+        initial.logOut();
+        initial.toSignPage();
+        signIn.signin(readerClass.readFromFile(1),readerClass.readFromFile(2));
+        initial.toUsersPage();
+        users.search("autoAccountHO@autoAccountHO.com");
+        users.activationDeaktivation();
+        initial.logOut();
+        initial.toSignPage();
+        signIn.signin(readerClass.readFromFile(3),readerClass.readFromFile(4));
+        initial.toPriceCalendar();
+        priceCalendar.checkButtonActivation();
     }
 
 
-    public void toUsersPage(){
-        adminButton.click();
-        usersButton.click();
+    @AfterTest
+    public void exit () {
+        //driver.quit();
     }
-
-    public void toActionsLog() throws InterruptedException {
-        adminButton.click();
-        Thread.sleep(100);
-        actionsLogButton.click();
-    }
-
-    public void logOut() throws InterruptedException {
-        Thread.sleep(100);
-        adminButton.click();
-        logOutButton.click();
-        Thread.sleep(100);
-    }
-
-
-
-    public void toPriceCalendar(){
-        priceCalendarButton.click();
-    }
-
-
-
-
 }
